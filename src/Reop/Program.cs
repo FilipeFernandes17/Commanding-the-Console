6266a1e42881429c78c2a674ee7e8e82bd80f7a6
FilipeFernandes17 <43007033+FilipeFernandes17@users.noreply.github.com>
2019-09-05 14:57:47 -0600

    Close #3

--- src/Reop/Program.cs
+++ src/Reop/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http; //for the HttpClient class
+using static System.Console; //for using the static methods of the console class without having to referance the class name
 
 namespace Reop
 {
     class Program
     {
+        const string API_BASE = "https://www.gitignore.io/api/";
+        const string API_LIST_LINES = API_BASE + "lsit?format=lines";
+
         static void Main(string[] args)
         {
+            var client = new HttpClient();
+            var response = client.GetAsync
+                (API_LIST_LINES).GetAwaiter().GetResult(); //run async code synchronously 
+            if(response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync
+                    ().GetAwaiter().GetResult();
+                WriteLine(content);//notice no console
+            }
         }
     }
 }
